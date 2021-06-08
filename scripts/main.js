67afe921bae36cc89ce61dc9ecdda0138319d384
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-06-08 21:01:31 +0200

    Use event target for setting color on path

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
 const picker = document.querySelector("#picker");
 let color = picker.value;
-const clsOne = document.querySelectorAll(".cls-1");
-picker.addEventListener("input", function() {
+const clsOne = document.querySelectorAll('.cls-1');
+const clearBtn = document.getElementById('clear-btn');
+
+picker.addEventListener('input', () => {
 color = picker.value;
 },false);
-for (let i = 0; i < clsOne.length; i++) {
+/*for (let i = 0; i < clsOne.length; i++) {
   clsOne[i].addEventListener('click', () => {
     clsOne[i].style.fill = color;
-    console.log(color);
   });
+}*/
+
+let selectedPath;
+
+const svg = document.querySelector('.illustration');
+svg.onclick = function(event) {
+  let target = event.target;
+  if (target.tagName != 'path') return;
+  highlight(target); 
+  console.log(target);
+};
+
+function highlight(path) {
+  selectedPath = path;
+  selectedPath.style.cssText=`fill: ${color}`;
 }
 
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  console.log('storage cleared')
+})
