eac8e24560dca46d4cb8e6fe579363c123079fa6
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-06-09 10:05:40 +0200

    Clean up a bit

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,28 @@ const picker = document.querySelector("#picker");
 let color = picker.value;
 const clsOne = document.querySelectorAll('.cls-1');
 const clearBtn = document.getElementById('clear-btn');
+const svg = document.querySelector('.illustration');
 
 picker.addEventListener('input', () => {
 color = picker.value;
 },false);
 
 let selectedPath;
+//let fillArr = [''];
 
-const svg = document.querySelector('.illustration');
 svg.onclick = function(event) {
   let target = event.target;
-  highlight(target); 
+  paint(target); 
   console.log(target);
+  //window.localStorage.setItem(selectedPath, color);
 };
 
-function highlight(path) {
+function paint(path) {
   selectedPath = path;
   selectedPath.style.cssText=`fill: ${color}`;
 }
+
+//window.localStorage.getItem(selectedPath);
 
 clearBtn.addEventListener('click', () => {
   localStorage.clear();
