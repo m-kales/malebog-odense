e7451cc1ea751db0d18df7a17a0d24f09f561f46
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-06-17 09:56:38 +0200

    Use functions instead of if elseif

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,8 @@ window.onload = function() {
   const clearBtn = document.getElementById('clear-btn');
   const svg = document.querySelector('.illustration');
   const showHex = document.querySelector("#show-hex");
-  const toggle = document.getElementById('toggle');
+  const btnRandom = document.getElementById('random');
+  const btnPicker = document.getElementById('pick-color');
   let selectedPath;
 
   svg.onclick = function clickSvg(event) {
@@ -18,12 +19,44 @@ window.onload = function() {
     selectedPath.classList.add('transition');
   }
   
-  toggle.addEventListener('change', (e) => {
-    this.checkboxValue = e.target.checked ? 'on' : 'off';
+randomStyle();
 
-    if (toggle.checked) {
-      console.log('if');
+    btnPicker.addEventListener('click', () => {
       color = picker.value;
+      btnPicker.classList.add('active-mode');
+      btnRandom.classList.remove('active-mode');
+      console.log('Picker clicked');
+      pickerStyle();
+    })
+  
+    btnRandom.addEventListener('click', () => {
+      btnRandom.classList.add('active-mode');
+      btnPicker.classList.remove('active-mode');
+      console.log('Random clicked');
+      randomStyle();
+    })
+  
+  function randomStyle() {
+    picker.classList.add('hide');
+    showHex.classList.remove('hide');
+      // random coloring
+      const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+        svg.addEventListener('click', () => {
+          let hexColor = "#";
+          for (let i = 0; i < 6; i++) {
+            hexColor += hex[getRandomNumber()];
+            color = hexColor;
+          }
+          
+          showHex.innerHTML = hexColor;
+          
+          function getRandomNumber() {
+            return Math.floor(Math.random() * hex.length);
+          }
+        })
+      }
+
+    function pickerStyle() {
       picker.addEventListener('input', () => {
       color = picker.value;
       }, false);
@@ -32,32 +65,8 @@ window.onload = function() {
       })
       picker.classList.remove('hide');
       showHex.classList.add('hide');
-      
-
-    } else if (!toggle.checked) {
-      console.log('else');
-
-      picker.classList.add('hide');
-      showHex.classList.remove('hide');
-        // random coloring
-        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-          svg.addEventListener('click', () => {
-            let hexColor = "#";
-            for (let i = 0; i < 6; i++) {
-              hexColor += hex[getRandomNumber()];
-              color = hexColor;
-            }
-            
-            
-            showHex.innerHTML = hexColor;
-            
-            function getRandomNumber() {
-              return Math.floor(Math.random() * hex.length);
-            }
-          })
     }
-    }
-  )
+
   // controls
   clearBtn.addEventListener('click', () => {
     location.reload();
