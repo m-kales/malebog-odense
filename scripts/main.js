7b93ad4e2dad0c7722034ac1b8ef1ac1d659025e
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-06-25 09:27:15 +0200

    Delete unused functions and variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,7 @@
 window.onload = function() {
-  const picker = document.querySelector("#picker");
-  //let color = 'picker.value';
   const clearBtn = document.getElementById('clear-btn');
   const svg = document.querySelector('.illustration');
-  const showHex = document.querySelector("#show-hex");
   const btnRandom = document.getElementById('random');
-  //const btnPicker = document.getElementById('pick-color');
   let selectedPath;
 
   svg.onclick = function clickSvg(event) {
@@ -18,23 +14,13 @@ window.onload = function() {
     selectedPath.style.cssText=`fill: ${color}`;
     selectedPath.classList.add('transition');
   }
-
-    /*btnPicker.addEventListener('click', () => {
-      color = picker.value;
-      btnPicker.classList.add('mode-active');
-      btnRandom.classList.remove('mode-active');
-      pickerStyle();
-    })*/
   
     btnRandom.addEventListener('click', () => {
       btnRandom.classList.add('mode-active');
-      //btnPicker.classList.remove('mode-active');
       randomStyle();
     })
   
   function randomStyle() {
-    //picker.classList.add('hide');
-    //showHex.classList.remove('hide');
       // random coloring
       const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
         svg.addEventListener('click', () => {
@@ -44,24 +30,11 @@ window.onload = function() {
             color = hexColor;
           }
           
-          //showHex.innerHTML = hexColor;
-          
           function getRandomNumber() {
             return Math.floor(Math.random() * hex.length);
           }
         })
       }
-
-    /*function pickerStyle() {
-      picker.addEventListener('input', () => {
-      color = picker.value;
-      }, false);
-      svg.addEventListener('click', () => {
-        color = picker.value;
-      })
-      picker.classList.remove('hide');
-      showHex.classList.add('hide');
-    }*/
 
   // controls
   clearBtn.addEventListener('click', () => {
